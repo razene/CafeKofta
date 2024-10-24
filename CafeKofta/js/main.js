1bba1565f9dd03d4a5c464ae7ba878cccf3a69ce
razene <koronax89@mail.ru>
2024-10-24 13:43:33 +0300

    Delete CafeKofta directory

--- CafeKofta/js/main.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.burger').addEventListener('click', function () {
-    document.querySelector('.burger span').classList.toggle('active');
-    document.querySelector('.burger-menu').classList.toggle('animate');
-});
